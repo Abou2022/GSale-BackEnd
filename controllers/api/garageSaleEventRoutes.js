352ee0696ae969c3d0df2ece60ac644ca2c174b0
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-06-07 10:36:51 -0700

    psuedo code for filter search of gse

--- controllers/api/garageSaleEventRoutes.js
+++ controllers/api/garageSaleEventRoutes.js
@@ -3,6 +3,7 @@
 const router = require("express").Router();
 const { Category, GarageSaleEvent, Vendor, Attendee, Comment } = require("../../models");
 const bearerToken = require("../../lib/bearer-auth-middleware");
+const { Op } = require('sequelize');
 
 // get all
 router.get('/', async (req, res) => {
@@ -38,18 +39,61 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// to do get by location
+
+// to do get by location, not all fields in the example below are required
+// format = /api/garageSaleEvents?startDate=11/11/2022&endDate=11/13/2022&lat=41.031031&lng=-121.054765&categories=antiques,furniture
+// req.body is an array of acceptable zip codes of location
+// router.post('/query', async (req, res) => {
+//     try {
+//         const startDate = req.query.startDate ? new Date(req.query.startDate) : null;
+//         const endDate = req.query.endDate ? new Date(req.query.endDate) : null;
+//         const lat = req.query.lat;
+//         const lng = req.query.lng;
+//         const categories = req.query.categories.split(',');
+
+//         GarageSaleEvent.findAll({where :  {startDate: {[Op.between] : [startDate, endDate ]}}})
+
+// start date they pick between gse start and end date OR end date they pick between gse start and end date
+
+
+
+// *** distance calculation ***
+// SELECT latitude, longitude, SQRT(
+//     POW(69.1 * (latitude - [startlat]), 2) +
+//     POW(69.1 * ([startlng] - longitude) * COS(latitude / 57.3), 2)) AS distance
+// FROM TableName HAVING distance < 25 ORDER BY distance;
+
+
+//         GarageSaleEvent.findAll({ where: }, { include: { all: true } }),
+//             Attendee.findAll({ where: { garageSaleEvent_id: req.params.id }, include: { all: true } }),
+//             Vendor.findAll({ where: { garageSaleEvent_id: req.params.id }, include: { all: true } }),
+//             Comment.findAll({ where: { garageSaleEvent_id: req.params.id }, include: { all: true } })
+
+//         const [garageSaleEvent, attendee, vendor, comment] = await Promise.all([
+//             GarageSaleEvent.findByPk(req.params.id, { include: { all: true } }),
+//             Attendee.findAll({ where: { garageSaleEvent_id: req.params.id }, include: { all: true } }),
+//             Vendor.findAll({ where: { garageSaleEvent_id: req.params.id }, include: { all: true } }),
+//             Comment.findAll({ where: { garageSaleEvent_id: req.params.id }, include: { all: true } })
+//         ]);
+//         garageSaleEvent === null ?
+//             res.status(404).json({ message: 'No garageSaleEvent with this id!' }) :
+//             res.status(200).json({ garageSaleEvent, attendee, vendor, comment });
+//     } catch (err) {
+//         console.log("err: ", err);
+//         res.status(500).json(err);
+//     }
+// });
 
 
 // post
 router.post('/', bearerToken, async (req, res) => {
     try {
-        const message = !req.body.creator_id ? 'expected a creator_id' 
-                : !req.body.address ? 'expected an address'
-                    : !req.body.zip ? 'expected an zip'
-                        : !req.body.lat ? 'expected an lat'
-                            : !req.body.lng ? 'expected an lng'
-                                : null;
+        const message = !req.body.creator_id ? 'expected a creator_id'
+            : !req.body.address ? 'expected an address'
+                : !req.body.zip ? 'expected an zip'
+                    : !req.body.lat ? 'expected an lat'
+                        : !req.body.lng ? 'expected an lng'
+                            : null;
         if (message)
             return res.status(400).json(`BAD REQUEST ERROR: ${message}`);
         const category = await Category.create();
